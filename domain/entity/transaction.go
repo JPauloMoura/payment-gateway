0c0b82071e09e7c89aa43947380dc7ba32e013a8
joao-paulomoura <joao.paulo@unico.io>
2021-12-06 23:27:06 -0300

    feat: :sparkles: cria entidade de transação e suas validações

---
+++ domain/entity/transaction.go
@@ -0,0 +1,36 @@
+package entity
+
+import (
+	"context"
+	"log"
+)
+
+// Transaction é a entidade padrão responsavel por uma transação
+type Transaction struct {
+	ID           string
+	AccountID    string
+	Amount       float64
+	CreditCard   CreditCard
+	Status       string
+	ErrorMessage string
+}
+
+// NewTransaction retorna uma nova entidade Transaction vazia
+func NewTransaction() *Transaction {
+	return &Transaction{}
+}
+
+// IsValid realiza a validação de uma entidade Transaction
+func (t Transaction) IsValid() error {
+	if t.Amount > 1000 || t.Amount < 1 {
+		log.Printf("context: %v, err: %s", context.Background(), ErrLimitForTransaction.Error())
+		return ErrLimitForTransaction
+	}
+
+	return nil
+}
+
+// SetCreditCard adiciona um cartão de credito a transação
+func (t *Transaction) SetCreditCard(card CreditCard) {
+	t.CreditCard = card
+}
